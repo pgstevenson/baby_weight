7364246835b5ff0e0bff1757ece03691fa4a48bd
Paul Stevenson <39392144+pgstevenson@users.noreply.github.com>
2021-06-05 15:23:27 +0800

    Update setup.sh
    By default, pulls images from Docker Hub rather than build.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-docker build -t pgstevenson/baby_weight -f ./webapp/Dockerfile .
-docker build -t pgstevenson/baby_weight_api ./api
+# build images if changes have been made:
+# docker build -t pgstevenson/baby_weight -f ./webapp/Dockerfile .
+# docker build -t pgstevenson/baby_weight_api ./api
+
+# pull baby_weight image from Docker Hub
+docker pull pgstevenson/baby_weight:latest
 
 docker-compose up -d
